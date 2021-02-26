94ebe21d16b2259322e390fbc1902c9cd2833f8e
iAmWasin <52755109+Domyni@users.noreply.github.com>
2021-02-26 13:53:28 +0800

    Go 7 #1 && Go 7 #2

--- main.go
+++ main.go
@@ -4,44 +4,42 @@ import "fmt"
 
 func main() {
 
-	/* Go 6 #1 Write a Go program that contains a function `isFizzBuzz`.
-	If the number is divisible by 3, then return "fizz".
-	If the number is divisible by 5, then return "buzz".
-	If the number is divisible by both 3 and 5, then return "fizzbuzz".
-	If the number doesn't match any of the conditions, then return "no go".
-	This time the function should loop from 1-100 and print the results accordingly. */
-	isFizzBuzz()
+	//	Given the following map
+	houses := map[string][]string{
+		"gryffindor": {"weasley", "hagrid", "dumbledore", "lupin"},
+		"hufflepuff": {"wenlock", "scamander", "helga", "diggory", "bobo"},
+		"ravenclaw":  {"flitwick", "bagnold", "wildsmith", "montmorency"},
+		"slytherin":  {"horace", "nigellus", "higgs", "bobo", "scorpius"},
+	}
 
-	// Go 6 #2
-	// Write a program that loops from 0-50 and prints the results of the current number in the iteration multiplied by 2.
-	// If the result of the multiplication is an odd number, defer printing the resulting. Otherwise, print it immediately.
-	// HINT: this is a trick question
-	deferOdd()
+	/*	Write a Go program that declares 4 blank slices for each of the keys in the map.
+		Go over the map and append/insert the values of each key as a separate element into their respective slices.
+		Once done, print the length of each of the slices. */
+	sliceLen(houses)
+
+	/* Write a Go program that loops from 0-15.
+	For each iteration, multiply the current iteration value by 2 and append the result to a slice.
+	Print the last 4 elements of the slice once done.
+	Use slice range (:) to print the elements. */
+	lastFour()
 }
 
-func isFizzBuzz() {
+func sliceLen(input map[string][]string) {
 
-	for n := 1; n <= 100; n++ {
-		if n%3 == 0 && n%5 == 0 {
-			fmt.Println(n, "fizzbuzz")
-		} else if n%3 == 0 {
-			fmt.Println(n, "fizz")
-		} else if n%5 == 0 {
-			fmt.Println(n, "buzz")
-		} else {
-			fmt.Println(n, "no go")
+	for _, v := range input {
+		var arr []interface{}
+		for _, elem := range v {
+			arr = append(arr, elem)
 		}
+		fmt.Println(len(arr))
 	}
 }
 
-func deferOdd() {
+func lastFour() {
 
-	for n := 0; n <= 50; n++ {
-		var result int = n * 2
-		if result%2 != 0 {
-			defer fmt.Println(result, "Odd")
-		} else {
-			fmt.Println(result, "Even")
-		}
+	var arr []interface{}
+	for i := 0; i <= 15; i++ {
+		arr = append(arr, i*2)
 	}
+	fmt.Println(arr[len(arr)-4:])
 }
